d5edd1d7b47b2e2bad3bb8a30a599d8e982e7125
WMB Admin <132984570+wemetbefore@users.noreply.github.com>
2025-02-13 22:34:47 +0200

    update the npm start redirection to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import ProtectedRoute from "./components/ProtectedRoute";
+import { AuthProvider } from "./context/AuthContext";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          {/* Redirect root URL to /login */}
+          <Route path="/" element={<Navigate to="/login" />} />
+
+          {/* Login Page */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Protected Dashboard Route */}
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 };
 
